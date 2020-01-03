b0f3054a72420485e64f1f412f85deca4ce44a07
Birgit Semsky <birgit.semsky@gmx.de>
2020-01-03 20:18:10 +0100

    add z-index from TopUp to 999

--- views/layout/TopUp.php
+++ views/layout/TopUp.php
@@ -1,3 +1,4 @@
 <button id="upButton" onclick="scrollToTop()" type="button"
-  style="position:fixed;right:15px; bottom:15px; display:none;"class="btn btn-outline-dark">TOP
+  style="position:fixed;right:15px; bottom:15px; display:none;
+  z-index:999; "class="btn btn-outline-dark">TOP
 </button>
